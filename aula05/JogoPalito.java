644209523eb63b02b814bfdff83d5f6f2a733db4
Marcelo <marc.m.amorim@gmail.com>
2026-09-24 10:48:40 -0300

    v1

--- aula05/JogoPalito.java
+++ aula05/JogoPalito.java
@@ -5,6 +5,7 @@ public class JogoPalito {
         Scanner sc = new Scanner(System.in);
         int qtdePJ = 3, qtdePC = 3;
         int qtdePMaoJ = 0, qtdePMaoC = 0; 
+        int chuteC = 0, chuteJ = 0;
 
         while(qtdePJ > 0 && qtdePC > 0) {
 
@@ -17,8 +18,31 @@ public class JogoPalito {
             qtdePMaoJ = sc.nextInt();
 
             qtdePMaoC = ((int)(Math.random()*100)) % (qtdePC+1);
-            
+
+            System.out.println("Qual seu chute?");
+            chuteJ = sc.nextInt();
+            chuteC = qtdePMaoC + ((int)(Math.random()*100)) % (qtdePJ+1);
+
+            System.out.println("----------");
+            System.out.println("Jogador chutou "+chuteJ);
+            System.out.println("Computador escondeu "+qtdePMaoC+" palitos");
+            System.out.println("Computador chutou "+chuteC);
+            System.out.println("----------");
+
+            if(qtdePMaoC+qtdePMaoJ == chuteJ) {
+                System.out.println("Vc ACERTOU!");
+                qtdePJ--;
+            } else {
+                if(qtdePMaoC+qtdePMaoJ == chuteC) {
+                    System.out.println("COMPUTADOR ACERTOU!");
+                    qtdePC--;
+                } else {
+                    System.out.println("NINGUÉM ACERTOU!");
+                }
+            }
         }
+
+        System.out.println("TERMINOU");
 
     }
 }
